9d750b79707fcdaa9aeddddba4706a8320b23cdd
a7md0 <a7m3d699@gmail.com>
2020-11-29 14:41:44 +0300

    Route urls and add view for gallery

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def gallery_list(request):
+    return HttpResponse("a HTML page should be returned")
