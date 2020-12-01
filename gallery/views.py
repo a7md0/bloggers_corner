7fcde8cb04b2e3b23ae473d63c25f7a89232bcde
a7md0 <a7m3d699@gmail.com>
2020-12-01 15:22:34 +0300

    Render dynamic page + image

--- gallery/views.py
+++ gallery/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from gallery.models import Gallery
 
 # Create your views here.
 def gallery_list(request):
-    return render(request, 'gallery/gallery.html')
+    gallery_objects = Gallery.objects.all()
+
+    return render(request, 'gallery/gallery.html', { 'gallery_objects': gallery_objects })
