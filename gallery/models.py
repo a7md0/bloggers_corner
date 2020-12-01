b88bcb4f688e2d3d5a414c3a3bab5c681c0c338b
a7md0 <a7m3d699@gmail.com>
2020-12-01 14:56:23 +0300

    Change image type in models of gallery

--- gallery/models.py
+++ gallery/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Gallery(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    image = models.FilePathField(path="/img")
+    image = models.CharField(max_length=100)
