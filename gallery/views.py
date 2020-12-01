5c36ab85e3fa37991b1da0db3be118a76cb1c510
a7md0 <a7m3d699@gmail.com>
2020-12-01 15:11:24 +0300

    Add gallery.html and render

--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def gallery_list(request):
-    return HttpResponse("a HTML page should be returned")
+    return render(request, 'gallery/gallery.html')
