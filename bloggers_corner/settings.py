3caafacf057fc04ab6291dda2fe2030c9d06e788
a7md0 <a7m3d699@gmail.com>
2020-12-01 14:43:32 +0300

    Finish lab9a

--- bloggers_corner/settings.py
+++ bloggers_corner/settings.py
@@ -76,8 +76,8 @@ WSGI_APPLICATION = 'bloggers_corner.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'bloggers_corner',
     }
 }
 
