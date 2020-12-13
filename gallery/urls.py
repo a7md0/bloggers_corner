4f9651da647843dd87feb4c0ba82472f2810eee1
a7md0 <a7m3d699@gmail.com>
2020-12-13 15:02:24 +0300

    Link the main template in the navbar

--- gallery/urls.py
+++ gallery/urls.py
@@ -3,6 +3,6 @@ from gallery import views
 
 app_name = "gallery"
 urlpatterns = [
-    path('', views.gallery_list),
+    path('', views.gallery_list, name="gallery_list"),
     path('<int:id>', views.gallery_detail, name="gallery_detail"),
 ]
