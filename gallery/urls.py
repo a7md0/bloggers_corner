c4d2a61064064c58aedb60684c39a2c0bc84a2ea
a7md0 <a7m3d699@gmail.com>
2020-12-13 14:46:14 +0300

    Add view gallery route & view & template

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from gallery import views
 
+app_name = "gallery"
 urlpatterns = [
     path('', views.gallery_list),
+    path('<int:id>', views.gallery_detail, name="gallery_detail"),
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -6,3 +6,8 @@ def gallery_list(request):
     gallery_objects = Gallery.objects.all()
 
     return render(request, 'gallery/gallery.html', { 'gallery_objects': gallery_objects })
+
+def gallery_detail(request, id):
+    gallery_item = Gallery.objects.get(id = id)
+
+    return render(request, 'gallery/detail.html', { 'gallery_item': gallery_item })
