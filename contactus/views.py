8113337ffbd7e748dd27411e6b199dc66945a92c
a7md0 <a7m3d699@gmail.com>
2020-12-15 15:22:17 +0300

    Implement contactus view method and render

--- contactus/views.py
+++ contactus/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from contactus.forms import ContactusForm
 
 # Create your views here.
 def contactus(request):
-    pass
+    myForm = ContactusForm()
+
+    return render(request, 'contactus/contactus.html', { 'myForm': myForm })
