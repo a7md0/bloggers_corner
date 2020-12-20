8e5fbfe1629b48777d1fcda235ae417ba7aa1d24
a7md0 <a7m3d699@gmail.com>
2020-12-20 15:20:45 +0300

    Add comment model

--- gallery/models.py
+++ gallery/models.py
@@ -5,3 +5,9 @@ class Gallery(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     image = models.CharField(max_length=100)
+
+class Comment(models.Model):
+    author = models.CharField(max_length=60)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    item = models.ForeignKey('Gallery', on_delete=models.CASCADE)
