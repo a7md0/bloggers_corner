7de3a06b08671a625925bc5ba8c513d79fdd86f8
a7md0 <a7m3d699@gmail.com>
2020-12-20 15:26:19 +0300

    Create Comment Form in gallery

---
+++ gallery/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.forms.fields import CharField
+
+
+class CommentForm(forms.Form):
+    author = CharField(
+        max_length=60,
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Your Name"
+            }
+        )
+    )
+    body = CharField(
+        widget=forms.Textarea(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Leave a  comment!"
+            }
+        )
+    )
