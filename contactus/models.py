d357b3508709362df480b382cc19c089b0603999
a7md0 <a7m3d699@gmail.com>
2020-12-20 15:05:58 +0300

    Contact us model

--- contactus/models.py
+++ contactus/models.py
@@ -1,3 +1,7 @@
-from django.db import models
+from djongo import models
 
 # Create your models here.
+class Contactus(models.Model):
+    name = models.CharField(max_length=60)
+    email = models.EmailField()
+    message = models.TextField()
