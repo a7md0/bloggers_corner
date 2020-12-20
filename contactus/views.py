c164c6b48e6bdcb0d4458886d15b5b542ed97437
a7md0 <a7m3d699@gmail.com>
2020-12-20 15:08:20 +0300

    Contactus view create model and save

--- contactus/views.py
+++ contactus/views.py
@@ -2,6 +2,7 @@ from django.http.request import HttpRequest
 from django.shortcuts import render
 from django.http import HttpResponse
 from contactus.forms import ContactusForm
+from contactus.models import Contactus
 
 # Create your views here.
 def contactus(request: HttpRequest):
@@ -11,6 +12,12 @@ def contactus(request: HttpRequest):
         myForm = ContactusForm(request.POST)
         if myForm.is_valid():
             name = myForm.cleaned_data['name']
+            email = myForm.cleaned_data['email']
+            message = myForm.cleaned_data['message']
+
+            contactus = Contactus(name = name, email = email, message = message)
+            contactus.save()
+
             notify = "Hi {}, we received your message".format(name)
 
             return HttpResponse(notify)
