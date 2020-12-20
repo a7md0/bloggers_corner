1517df1749fa39b47cfc95c1969e794ed726b426
a7md0 <a7m3d699@gmail.com>
2020-12-20 15:41:31 +0300

    Add Comment model to admin

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from gallery.models import Gallery
+from gallery.models import Gallery, Comment
 
 # Register your models here.
 admin.site.register(Gallery)
+admin.site.register(Comment)
