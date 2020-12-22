1c5e17295a0148371cc8c769dfa8ee636ff85716
a7md0 <a7m3d699@gmail.com>
2020-12-22 14:57:09 +0300

    Override login/logout redirect links

--- bloggers_corner/settings.py
+++ bloggers_corner/settings.py
@@ -129,3 +129,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL = "gallery:gallery_list"
+LOGOUT_REDIRECT_URL = "gallery:gallery_list"
